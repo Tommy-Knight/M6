da8a4ede4c87d30aa61dd3b65df0342171ffd8af
Tommy Knight <78544868+Tommy-Knight@users.noreply.github.com>
2021-08-24 16:01:32 +0100

    bcryptpasswordhashing

--- MongoBlog/src/services/authors/schema.js
+++ MongoBlog/src/services/authors/schema.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import createError from "http-errors";
+import bcrypt from "bcrypt";
 
 const { Schema, model } = mongoose;
 
@@ -13,6 +14,51 @@ const AuthorsSchema = new Schema(
 	},
 	{ timestamps: true }
 );
+
+AuthorsSchema.pre("save", async function (next) {
+	// BEFORE saving new user in db, hash the password
+	const newUser = this;
+
+	const plainPW = newUser.password;
+
+	if (newUser.isModified("password")) {
+		newUser.password = await bcrypt.hash(plainPW, 10);
+	}
+
+	next();
+});
+
+AuthorsSchema.methods.toJSON = function () {
+	// toJSON is called every time express does a res.send
+
+	const userDocument = this;
+
+	const userObject = userDocument.toObject();
+
+	delete userObject.password;
+
+	delete userObject.__v;
+
+	return userObject;
+};
+
+AuthorsSchema.statics.checkCredentials = async function (email, plainPW) {
+	// 1. find user in db by email
+
+	const user = await this.findOne({ email });
+
+	if (user) {
+		// 2. if user is found we need to compare plainPW with hashed PW
+		const isMatch = await bcrypt.compare(plainPW, user.password);
+
+		// 3. return a meaningful response
+
+		if (isMatch) return user;
+		else return null;
+	} else {
+		return null;
+	}
+};
 
 AuthorsSchema.post("validate", (error, doc, next) => {
 	if (error) {
